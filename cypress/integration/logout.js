7101d2d5f578f87eadedd8f59beea91c502c76c0
mira-kargo <miraraudhatuljannah98@gmail.com>
2022-10-03 00:07:20 +0700

    x

--- cypress/integration/logout.js
+++ cypress/integration/logout.js
@@ -1,12 +1,28 @@
-let username = "root";
-let password = "root123";
+const logout = "https://qa-interview.srcli.xyz/logout"
+const login = "https://qa-interview.srcli.xyz/login"
 
-cy.session(username, () => {
-    cy.request({
-      method: 'POST',
-      url: 'https://qa-interview.srcli.xyz/login',
-      body: { username, password },
-    }).then(({ body }) => {
-      window.localStorage.setItem('authToken', body.token)
-    })
-  })
+it("Logout after login", () =>  {
+
+    cy.visit(login)
+    cy.get('[type="text"]').type("root")
+    cy.get('[type="password"]').type("root123")
+    cy.get('[type="submit"]').click()
+    cy.clearCookies()
+
+              cy.request({
+
+                  url: logout,
+                  followRedirect: false,
+                  failOnStatusCode: false
+                  ,
+              }).then((resp) => {
+                  expect(resp.status).to.eq(200);
+                  cy.clearCookies()
+
+                  cy.contains("Welcome!").should("exist");
+                  cy.wait(3000)
+            cy.visit(login)
+
+    });
+
+});
